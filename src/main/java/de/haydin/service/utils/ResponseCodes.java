3ae29149543b88e83ac7488d6a64403823234dfa
aydins <aydins@aydins-ThinkPad-X230>
2017-08-22 23:04:25 +0200

    Nur ein Kommentar

--- src/main/java/de/haydin/service/utils/ResponseCodes.java
+++ src/main/java/de/haydin/service/utils/ResponseCodes.java
@@ -10,5 +10,5 @@ public class ResponseCodes {
     public final static int DATABSEERROR = 512;
     public final static int SQLERROR = 513;
     public final static int IOERROR = 514;
-    
+    //Nur ein Kommentar
 }
