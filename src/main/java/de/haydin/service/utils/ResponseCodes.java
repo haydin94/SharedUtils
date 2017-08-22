1492a365285ba07bad42b235efcac6bbc5b009da
aydins <aydins@aydins-ThinkPad-X230>
2017-08-22 22:51:12 +0200

    IOERROR hinzugefügt

--- src/main/java/de/haydin/service/utils/ResponseCodes.java
+++ src/main/java/de/haydin/service/utils/ResponseCodes.java
@@ -9,5 +9,6 @@ public class ResponseCodes {
     public final static int EMPTYBODYERROR = 418;
     public final static int DATABSEERROR = 512;
     public final static int SQLERROR = 513;
+    public final static int IOERROR = 514;
     
 }
