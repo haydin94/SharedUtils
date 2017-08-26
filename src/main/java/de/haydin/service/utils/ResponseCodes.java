d172a20e61191d645069a860e2945c136f986fdb
aydins <aydins@aydins-ThinkPad-X230>
2017-08-26 12:46:33 +0200

    InvalidBodyError

--- src/main/java/de/haydin/service/utils/ResponseCodes.java
+++ src/main/java/de/haydin/service/utils/ResponseCodes.java
@@ -10,5 +10,7 @@ public class ResponseCodes {
     public final static int DATABSEERROR = 512;
     public final static int SQLERROR = 513;
     public final static int IOERROR = 514;
+    public final static int INVALIDBODYERROR = 516;
+    
     //Nur ein Kommentar
 }
